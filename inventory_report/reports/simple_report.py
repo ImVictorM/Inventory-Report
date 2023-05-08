81ce6cc56457b9490f59a97e14dbd2319fd9075e
Victor Mendes <victorutdsim7@gmail.com>
2023-05-08 19:45:35 -0300

    refactor: logica do inventario da classe simple_report

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -2,42 +2,49 @@ from datetime import datetime
 
 
 class SimpleReport:
+    inventory = []
+
     @classmethod
-    def get_oldest_created_date(cls, inventory):
+    def get_oldest_created_date(cls):
         manufacturing_dates = [
-            item["data_de_fabricacao"] for item in inventory
+            item["data_de_fabricacao"] for item in cls.inventory
         ]
         oldest_created_date = min(manufacturing_dates)
         return oldest_created_date
 
     @classmethod
-    def get_closest_expiration_date(cls, inventory):
+    def get_closest_expiration_date(cls):
         now = datetime.today()
+
         valid_expiration_dates = [
             item["data_de_validade"]
-            for item in inventory
+            for item in cls.inventory
             if datetime.strptime(item["data_de_validade"], "%Y-%m-%d") >= now
         ]
+
         closest_expiration_date = min(
             valid_expiration_dates,
             key=lambda date: abs(datetime.strptime(date, "%Y-%m-%d") - now),
         )
+
         return closest_expiration_date
 
     @classmethod
-    def company_products_quantity(cls, inventory):
+    def company_products_quantity(cls, inventory=[]):
         company_products = {}
+        inventory_to_use = inventory if inventory else cls.inventory
 
-        for item in inventory:
+        for item in inventory_to_use:
             if item["nome_da_empresa"] in company_products:
                 company_products[item["nome_da_empresa"]] += 1
             else:
                 company_products[item["nome_da_empresa"]] = 1
+
         return company_products
 
     @classmethod
-    def get_company_with_more_products(cls, inventory):
-        company_products = SimpleReport.company_products_quantity(inventory)
+    def get_company_with_more_products(cls):
+        company_products = cls.company_products_quantity()
 
         company_with_more_products = max(
             company_products, key=company_products.get
@@ -47,13 +54,11 @@ class SimpleReport:
 
     @classmethod
     def generate(cls, inventory):
-        oldest_created_date = SimpleReport.get_oldest_created_date(inventory)
-        closest_expiration_date = SimpleReport.get_closest_expiration_date(
-            inventory
-        )
-        company_with_more_products = (
-            SimpleReport.get_company_with_more_products(inventory)
-        )
+        cls.inventory = inventory
+
+        oldest_created_date = cls.get_oldest_created_date()
+        closest_expiration_date = cls.get_closest_expiration_date()
+        company_with_more_products = cls.get_company_with_more_products()
 
         return (
             f"Data de fabricação mais antiga: {oldest_created_date}\n"
