d2d4086c004b8cd85e7444acc2716058b3124009
Victor Mendes <victorutdsim7@gmail.com>
2023-05-08 18:00:39 -0300

    [req-5] feat: criação relatórios de arquivos json

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,18 +1,25 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
     @classmethod
-    def import_data(cls, csv_path, report_type):
-        with open(csv_path) as csv_file:
-            reader = csv.DictReader(csv_file)
-            inventory = [row for row in reader]
-            if report_type == 'simples':
+    def import_data(cls, file_path, report_type):
+        with open(file_path) as file:
+            inventory = []
+
+            if file_path.endswith(".csv"):
+                reader = csv.DictReader(file)
+                inventory = [row for row in reader]
+            elif file_path.endswith(".json"):
+                inventory = json.load(file)
+
+            if report_type == "simples":
                 return SimpleReport.generate(inventory)
             else:
                 return CompleteReport.generate(inventory)
 
 
-Inventory.import_data('inventory_report/data/inventory.csv', 'simples')
+Inventory.import_data("inventory_report/data/inventory.json", "simples")
