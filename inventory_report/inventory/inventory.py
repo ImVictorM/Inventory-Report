2113a2ba5807319f266022b0cc89d74775d979a6
Victor Mendes <victorutdsim7@gmail.com>
2023-05-08 18:38:30 -0300

    refactor: criação de relatorios por tipo de arquivo

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -7,31 +7,46 @@ from inventory_report.reports.complete_report import CompleteReport
 
 class Inventory:
     @classmethod
+    def csv_to_list(cls, file):
+        reader = csv.DictReader(file)
+        inventory = [row for row in reader]
+        return inventory
+
+    @classmethod
+    def json_to_list(cls, file):
+        inventory = json.load(file)
+        return inventory
+
+    @classmethod
+    def xml_to_list(cls, file_path):
+        tree = ET.parse(file_path)
+        dataset = tree.getroot()
+        xml_to_list = []
+
+        for record in dataset:
+            item = {}
+            for child in record:
+                item[child.tag] = child.text
+
+            xml_to_list.append(item)
+
+        return xml_to_list
+
+    @classmethod
     def import_data(cls, file_path, report_type):
         with open(file_path) as file:
             inventory = []
 
             if file_path.endswith(".csv"):
-                reader = csv.DictReader(file)
-                inventory = [row for row in reader]
+                inventory = Inventory.csv_to_list(file)
             elif file_path.endswith(".json"):
-                inventory = json.load(file)
+                inventory = Inventory.json_to_list(file)
             else:
                 # xml case
-                tree = ET.parse(file_path)
-                dataset = tree.getroot()
-                xml_to_list = []
-
-                for record in dataset:
-                    item = {}
-                    for child in record:
-                        item[child.tag] = child.text
-
-                    xml_to_list.append(item)
-
-                inventory = xml_to_list
+                inventory = Inventory.xml_to_list(file_path)
 
             if report_type == "simples":
                 return SimpleReport.generate(inventory)
             else:
+                # complete case
                 return CompleteReport.generate(inventory)
