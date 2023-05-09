436868b6e9c3715542f02ddf2d6bca6c10a959f4
Victor Mendes <victorutdsim7@gmail.com>
2023-05-09 15:08:26 -0300

    [req-11] feat: função main para gerar relatorio por cli

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,26 @@
+import sys
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.importer import Importer
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+
+
 def main():
-    pass
+    if len(sys.argv) != 3:
+        sys.stderr.write("Verifique os argumentos\n")
+    else:
+        FILE_TYPE_IMPORTER_MAP = {
+            ".csv": CsvImporter,
+            ".json": JsonImporter,
+            ".xml": XmlImporter,
+        }
+        _, file_path, report_type = sys.argv
+        file_extension = file_path[file_path.rfind("."):]
+        importer: Importer = FILE_TYPE_IMPORTER_MAP.get(file_extension)
+
+        report = InventoryRefactor(importer).import_data(
+            file_path, report_type
+        )
+
+        sys.stdout.write(report)
